4b0a2478bc182307ba2b8226546ec11175ec76e5
Saurabh <251536437+Easyskill-in@users.noreply.github.com>
2026-03-19 10:34:58 +0530

    Enhance User model with password hashing
    Added password hashing and comparison methods to user schema.

--- Mongo1/model/User.model.js
+++ Mongo1/model/User.model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, require: true },
+    username: { type: String, required: true },
     password: String,
     email: String,
     age: Number,
@@ -9,7 +10,49 @@ const userSchema = new mongoose.Schema({
     timestamps: true
 });
 
-const User = mongoose.model('User', userSchema);
+userSchema.pre("save", async function (next) {
+
+    if (!this.isModified('password')) {
+        return next()
+    }
+
+    try {
+        this.password = await bcrypt.hash(this.password, 7)
+        next()
+    } catch (error) {
+        return error.message
+    }
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    try {
+        const result = await bcrypt.compare(password, this.password)
+        return result || true
+    } catch (error) {
+        console.log("Error : ", error.message)
+        return false
+    }
+}
+
+
+// const User = mongoose.model('User', userSchema);
 
 // module.exports = User;
 module.exports = mongoose.model('User', userSchema);
+
+---------------------------------------------
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     username: { type: String, require: true },
+//     password: String,
+//     email: String,
+//     age: Number,
+// }, {
+//     timestamps: true
+// });
+
+// const User = mongoose.model('User', userSchema);
+
+// // module.exports = User;
+// module.exports = mongoose.model('User', userSchema);
